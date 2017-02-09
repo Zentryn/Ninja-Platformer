2b9af1ce0e6cc26b468d554ac63686694eb87eb2
Zentryn <veikkis98@gmail.com>
2017-02-09 22:46:07 +0200

    Added simple Box2D boxes

--- Ninja Platformer/GameplayScreen.h
+++ Ninja Platformer/GameplayScreen.h
@@ -1,11 +1,20 @@
 #pragma once
 
+#include <Box2D/Box2D.h>
 #include <Bengine/IGameScreen.h>
+#include <Bengine/SpriteBatch.h>
+#include <Bengine/GLSLProgram.h>
+#include <Bengine/Camera2D.h>
+#include <Bengine/Window.h>
+#include <Bengine/GLTexture.h>
+#include <memory>
+#include "Box.h"
+#include <vector>
 
 class GameplayScreen : public Bengine::IGameScreen
 {
 public:
-    GameplayScreen();
+    GameplayScreen(Bengine::Window* window);
     ~GameplayScreen();
 
     virtual int getNextScreenIndex() const override;
@@ -26,4 +35,13 @@ public:
 
 private:
     void checkInput();
+
+    Bengine::SpriteBatch m_spriteBatch;
+    Bengine::GLSLProgram m_textureProgram;
+    Bengine::Camera2D m_camera;
+    Bengine::GLTexture m_texture;
+    Bengine::Window* m_window;
+
+    std::vector<Box> m_boxes;
+    std::unique_ptr<b2World> m_world;
 };
