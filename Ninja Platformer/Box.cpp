b0dd91f9aa04357599b4194ec6c2a4a612781d92
Zentryn <veikkis98@gmail.com>
2017-02-10 00:14:23 +0200

    Moved box drawing to the box class

--- Ninja Platformer/Box.cpp
+++ Ninja Platformer/Box.cpp
@@ -12,15 +12,24 @@ Box::~Box()
 }
 
 
-void Box::init(b2World* world, const glm::vec2& position, const glm::vec2& dimensions, Bengine::ColorRGBA8 color)
+void Box::init(b2World* world,
+               const glm::vec2& position,
+               const glm::vec2& dimensions,
+               Bengine::GLTexture texture,
+               Bengine::ColorRGBA8 color,
+               bool fixedRotation /*= false*/,
+               glm::vec4 uvRect /*= glm::vec4(0.0f, 0.0f, 1.0f, 1.0f)*/)
 {
     m_color = color;
     m_dimensions = dimensions;
+    m_texture = texture;
+    m_uvRect = uvRect;
 
     // Make the body
     b2BodyDef bodyDef;
     bodyDef.type = b2_dynamicBody;
     bodyDef.position.Set(position.x, position.y);
+    bodyDef.fixedRotation = fixedRotation;
     m_body = world->CreateBody(&bodyDef);
 
     b2PolygonShape boxShape;
@@ -32,4 +41,22 @@ void Box::init(b2World* world, const glm::vec2& position, const glm::vec2& dimen
     fixtureDef.density = 1.0f;
     fixtureDef.friction = 0.3f;
     m_fixture = m_body->CreateFixture(&fixtureDef);
+}
+
+void Box::draw(Bengine::SpriteBatch& spriteBatch)
+{
+    glm::vec4 destRect(
+        m_body->GetPosition().x - m_dimensions.x / 2.0f,
+        m_body->GetPosition().y - m_dimensions.y / 2.0f,
+        m_dimensions
+    );
+
+    spriteBatch.draw(
+        destRect,
+        m_uvRect,
+        m_texture.id,
+        0.0f,
+        m_color,
+        m_body->GetAngle()
+    );
 }
