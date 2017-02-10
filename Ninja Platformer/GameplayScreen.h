9cb1f4bc0e683a76c7eb9b9bbc421bbe276aea8c
Zentryn <veikkis98@gmail.com>
2017-02-10 02:34:35 +0200

    Added collision boxes

--- Ninja Platformer/GameplayScreen.h
+++ Ninja Platformer/GameplayScreen.h
@@ -7,6 +7,8 @@
 #include <Bengine/Camera2D.h>
 #include <Bengine/Window.h>
 #include <Bengine/GLTexture.h>
+#include <Bengine/SpriteFont.h>
+#include <Bengine/DebugRenderer.h>
 #include <memory>
 #include "Box.h"
 #include "Player.h"
@@ -38,10 +40,14 @@ private:
     void checkInput();
 
     Bengine::SpriteBatch m_spriteBatch;
+    std::unique_ptr<Bengine::SpriteFont> m_spriteFont;
     Bengine::GLSLProgram m_textureProgram;
     Bengine::Camera2D m_camera;
     Bengine::GLTexture m_texture;
     Bengine::Window* m_window;
+    Bengine::DebugRenderer m_debugRenderer;
+
+    bool m_renderDebug = true;
 
     Player m_player;
     std::vector<Box> m_boxes;
